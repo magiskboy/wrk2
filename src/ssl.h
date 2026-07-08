52e2bbc1166634deab43d58356dd85c9741bf532
Nguyễn Khắc Thành <nguyenkhacthanh244@gmail.com>
2026-07-08 19:48:29 +0700

    Remove ssl_write declaration
    Remove ssl_write function declaration from ssl.h

--- src/ssl.h
+++ src/ssl.h
@@ -8,7 +8,6 @@ SSL_CTX *ssl_init();
 status ssl_connect(connection *, char *);
 status ssl_close(connection *);
 status ssl_read(connection *, size_t *);
-status ssl_write(connection *, char *, size_t, size_t *);
 size_t ssl_readable(connection *);
 
 #endif /* SSL_H */
